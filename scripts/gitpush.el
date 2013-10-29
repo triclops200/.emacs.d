c506c7a7b0812aecc973b7aef58e0ff0ffae74e8
Bryan Hoyle <starfoxprime@gmail.com>
2013-10-29 11:03:12 -0400

    More git commands

--- scripts/gitpush.el
+++ scripts/gitpush.el
@@ -1,8 +1,21 @@
-(defun git-push-all (commit-message)
+
+(defun git-commit (commit-message)
   (interactive "sCommit Message: ")
-  (shell-command "git add -A")
-  (shell-command (concat "git commit -m \"" commit-message "\""))
+  (shell-command (concat "git commit -m\""commit-message"\"")))
+
+(defun git-add-all ()
+  (interactive)
+  (shell-command "git add -A"))
+
+(defun git-push ()
+  (interactive)
   (save-window-excursion
 	(let ((buf (generate-new-buffer "async")))
-	  (async-shell-command  "git push origin master &" buf)
+	  (async-shell-command  "git push origin master" buf)
 	  (run-with-timer 10 nil (lambda (buf) (kill-buffer buf)) buf))))
+
+(defun git-push-all (commit-message)
+  (interactive "sCommit Message: ")
+  (git-add-all)
+  (git-commit commit-message)
+  (git-push))
