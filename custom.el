c86405f53c87777d44db4019f62ffb957cac83c7
Bryan Hoyle <starfoxprime@gmail.com>
2014-01-26 14:12:56 -0500

    Better custom.el

--- custom.el
+++ custom.el
@@ -1,30 +1,39 @@
-;;(package-initialize)
-;;(require 'package)
+(require 'package)
+(package-initialize)
+(set-default-font "-bitstream-Bitstream Vera Sans Mono-normal-normal-normal-monospace-13-*-*-*-m-0-iso10646-1")
+(add-to-list 'custom-theme-load-path
+	     "~/.emacs.d/emacs-color-theme-solarized")
+(load-theme 'solarized-dark t)
+(add-to-list 'load-path "~/.emacs.d")
 (defun fullpath-relative-to-current-file (file-relative-path)
-<<<<<<< HEAD
   (concat (file-name-directory (or load-file-name buffer-file-name)) file-relative-path))
-=======
-  (concat (file-name-directory (or load-file-name buffer-file-name)) file-relative-path)
-  )
->>>>>>> cd9f28ca2e1c34a8b3c42fd022d93a14906d97f0
+
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
 
-(defvar my-packages '(
-					  clojure-mode
-					  clojure-test-mode
-					  cider
-					  company
-					  org
-					  solarized-theme
-					  paredit
-					  autocomplete))
 
+
+(defvar my-packages '(clojure-mode
+		      clojure-test-mode
+		      cider
+		      company
+		      org
+		      paredit
+		      auto-complete
+		      slime
+		      emacs-eclim))
+(defvar refresh t)
 (dolist (p my-packages)
   (when (not (package-installed-p p))
-	(package-install p)))
+    (progn (if refresh
+	   (progn
+	     (setq refresh nil)
+	     (package-refresh-contents)))
+    (package-install p))))
 
-;;(package-initialize)
 (setq inferior-lisp-program "sbcl")
 (add-to-list 'load-path (fullpath-relative-to-current-file "slime"))
 (require 'slime-autoloads)
@@ -69,7 +78,7 @@
 (defun eclim-run-test ()
   (interactive)
   (if (not (string= major-mode "java-mode"))
-	  (message "Sorry cannot run current buffer."))
+      (message "Sorry cannot run current buffer."))
   (compile (concat eclim-executable " -command java_junit -p " eclim--project-name " -t " (eclim-package-and-class))))
 (add-hook 'after-init-hook 'global-company-mode)
 
@@ -80,9 +89,9 @@
 
 (defface esk-paren-face
   '((((class color) (background dark))
-	 (:foreground "grey50"))
-	(((class color) (background light))
-	 (:foreground "grey55")))
+     (:foreground "grey50"))
+    (((class color) (background light))
+     (:foreground "grey55")))
   "Face used to dim parentheses."
   :group 'starter-kit-faces)
 
@@ -97,42 +106,34 @@
 (add-to-list 'auto-mode-alist '("\\.sml\\'" . scheme-mode))
 (unless window-system
   (when (getenv "DISPLAY")
-	;; Callback for when user cuts
-	(defun xsel-cut-function (text &optional push)
-	  ;; Insert text to temp-buffer, and "send" content to xsel stdin
-	  (with-temp-buffer
-		(insert text)
-		;; I prefer using the "clipboard" selection (the one the
-		;; typically is used by c-c/c-v) before the primary selection
-		;; (that uses mouse-select/middle-button-click)
-		(call-process-region (point-min) (point-max) "xsel" nil 0 nil "--clipboard" "--input")))
-	;; Call back for when user pastes
-	(defun xsel-paste-function()
-	  ;; Find out what is current selection by xsel. If it is different
-	  ;; from the top of the kill-ring (car kill-ring), then return
-	  ;; it. Else, nil is returned, so whatever is in the top of the
-	  ;; kill-ring will be used.
-	  (let ((xsel-output (shell-command-to-string "xsel --clipboard --output")))
-		(unless (string= (car kill-ring) xsel-output)
-		  xsel-output )))
-	;; Attach callbacks to hooks
-	(setq interprogram-cut-function 'xsel-cut-function)
-	(setq interprogram-paste-function 'xsel-paste-function)
-	(menu-bar-mode -1)))
+    ;; Callback for when user cuts
+    (defun xsel-cut-function (text &optional push)
+      ;; Insert text to temp-buffer, and "send" content to xsel stdin
+      (with-temp-buffer
+	(insert text)
+	;; I prefer using the "clipboard" selection (the one the
+	;; typically is used by c-c/c-v) before the primary selection
+	;; (that uses mouse-select/middle-button-click)
+	(call-process-region (point-min) (point-max) "xsel" nil 0 nil "--clipboard" "--input")))
+    ;; Call back for when user pastes
+    (defun xsel-paste-function()
+      ;; Find out what is current selection by xsel. If it is different
+      ;; from the top of the kill-ring (car kill-ring), then return
+      ;; it. Else, nil is returned, so whatever is in the top of the
+      ;; kill-ring will be used.
+      (let ((xsel-output (shell-command-to-string "xsel --clipboard --output")))
+	(unless (string= (car kill-ring) xsel-output)
+	  xsel-output )))
+    ;; Attach callbacks to hooks
+    (setq interprogram-cut-function 'xsel-cut-function)
+    (setq interprogram-paste-function 'xsel-paste-function)
+    (menu-bar-mode -1)))
 (global-linum-mode t)
-<<<<<<< HEAD
-
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 (add-hook 'cider-mode-hook 'paredit-mode)
-(add-hook 'clojure-mode-hook 'paredit-mode)
-
-
-
-
-=======
 (defun my:ac-c-headers-init ()
-    (require 'auto-complete-c-headers)
-	  (add-to-list 'ac-sources 'ac-source-c-headers))
+  (require 'auto-complete-c-headers)
+  (add-to-list 'ac-sources 'ac-source-c-headers))
 
 (add-hook 'c++-mode-hook 'my:ac-c-headers-init)
 (add-hook 'c-mode-hook 'my:ac-c-headers-init)
@@ -143,26 +144,22 @@
  python-shell-prompt-regexp "In \\[[0-9]+\\]: "
  python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
  python-shell-completion-setup-code
-   "from IPython.core.completerlib import module_completion"
+ "from IPython.core.completerlib import module_completion"
  python-shell-completion-module-string-code
-   "';'.join(module_completion('''%s'''))\n"
+ "';'.join(module_completion('''%s'''))\n"
  python-shell-completion-string-code
-   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+ "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
 
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 (add-hook 'lisp-mode-hook #'enable-paredit-mode)
 (add-hook 'clojure-mode-hook #'enable-paredit-mode)
->>>>>>> cd9f28ca2e1c34a8b3c42fd022d93a14906d97f0
 (require 'org)
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
-<<<<<<< HEAD
 (setq org-agenda-files
       (list "~/todo/main.org"))
 (add-hook 'org-mode-hook 'org-indent-mode)
-=======
-(setq org-agenda-files (list "~/todo/main.org"))
 (defun replace-last-sexp ()
   (interactive)
   (let ((value (eval (preceding-sexp))))
@@ -171,17 +168,35 @@
 (define-key global-map "\C-x\C-j" 'replace-last-sexp)
 
 (defun erc-my-privmsg-sound (proc parsed)
-    (let* ((tgt (car (erc-response.command-args parsed)))
-           (privp (erc-current-nick-p tgt)))
-      (and
-       privp
-       (ding)
-       nil)))
+  (let* ((tgt (car (erc-response.command-args parsed)))
+	 (privp (erc-current-nick-p tgt)))
+    (and
+     privp
+     (ding)
+     nil)))
 
- (add-hook 'erc-server-PRIVMSG-functions
-            'erc-my-privmsg-sound)
+(add-hook 'erc-server-PRIVMSG-functions
+	  'erc-my-privmsg-sound)
 (add-hook 'erc-insert-post-hook 
-              (lambda () (goto-char (point-min)) 
-                (when (re-search-forward
-                       (regexp-quote  (erc-current-nick)) nil t) (ding))))
->>>>>>> cd9f28ca2e1c34a8b3c42fd022d93a14906d97f0
+	  (lambda () (goto-char (point-min)) 
+	    (when (re-search-forward
+		   (regexp-quote  (erc-current-nick)) nil t) (ding))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#262626" "#d70000" "#5f8700" "#af8700" "#0087ff" "#af005f" "#00afaf" "#626262"])
+ '(background-color nil)
+ '(background-mode dark)
+ '(cursor-color nil)
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
+ '(foreground-color nil)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
