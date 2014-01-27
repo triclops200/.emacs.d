d2b3f77d8f892d892a7b3520f6f0146aa6e169fb
Bryan Hoyle <starfoxprime@gmail.com>
2014-01-27 17:11:44 -0500

    stuf

--- custom.el
+++ custom.el
@@ -197,7 +197,8 @@
  '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
  '(foreground-color nil)
  '(show-paren-mode t)
- '(tool-bar-mode nil))
+ '(tool-bar-mode nil)
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -205,3 +206,4 @@
  ;; If there is more than one, they won't work right.
  )
 (load "znc_servers.el")
+
