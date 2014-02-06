90f1d7cc0eee36645a5705e0aa624467a1eea550
Bryan Hoyle <starfoxprime@gmail.com>
2014-02-05 22:21:26 -0500

    Fixed errors!

--- custom.el
+++ custom.el
@@ -3,12 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#262626" "#d70000" "#5f8700" "#af8700" "#0087ff" "#af005f" "#00afaf" "#626262"])
+ '(ansi-color-names-vector
+   ["#262626" "#d70000" "#5f8700" "#af8700" "#0087ff" "#af005f" "#00afaf" "#626262"])
  '(background-color nil)
  '(background-mode dark)
  '(cursor-color nil)
- '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
+ '(custom-safe-themes
+   (quote
+    ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
  '(foreground-color nil)
+ '(haskell-mode-hook (quote (turn-on-haskell-indentation)))
  '(send-mail-function (quote smtpmail-send-it))
  '(show-paren-mode t)
  '(smtpmail-smtp-server "smtp.googlemail.com")
@@ -30,7 +34,7 @@
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 (add-to-list 'load-path "~/.emacs.d/lisp/secrets/")
 (load "my-packages.el")
-(load "customs-scripts.el")
+(load "custom-scripts.el")
 (load "eclim-stuff.el")
 (load "my-functions.el")
 (load "slime-stuff.el")
