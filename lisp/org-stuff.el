341c7029b4bb5a31f7ad56dc076a4eb4ad8f389d
Bryan Hoyle <starfoxprime@gmail.com>
2014-04-28 14:33:17 -0400

    Styles

--- lisp/org-stuff.el
+++ lisp/org-stuff.el
@@ -53,3 +53,24 @@
 		(org . t)
 		(scheme . t)
 		(sh . t)))
+
+(defun my-org-inline-css-hook (exporter)
+  "Insert custom inline css"
+  (when (eq exporter 'html)
+    (let* ((dir (ignore-errors (file-name-directory (buffer-file-name))))
+           (path (concat dir "style.css"))
+           (homestyle (or (null dir) (null (file-exists-p path))))
+           (final (if homestyle "~/.emacs.d/org-style.css" path)))
+      (setq org-html-head-include-default-style nil)
+      (setq org-html-head (concat
+                           "<style type=\"text/css\">\n"
+                           "<!--/*--><![CDATA[/*><!--*/\n"
+                           (with-temp-buffer
+                             (insert-file-contents final)
+                             (buffer-string))
+                           "/*]]>*/-->\n"
+                           "</style>\n")))))
+
+(eval-after-load 'ox
+  '(progn
+     (add-hook 'org-export-before-processing-hook 'my-org-inline-css-hook)))
